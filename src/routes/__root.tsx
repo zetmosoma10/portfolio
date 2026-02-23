c9ea30b5d945dabdb12d008cd314302d9ea79944
Zet Mosoma <zetmosoma10@gmail.com>
2026-02-23 19:22:10 +0200

    add meta data for SEO

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -15,14 +15,82 @@ export const Route = createRootRoute({
         name: "viewport",
         content: "width=device-width, initial-scale=1",
       },
+
+      // Primary SEO
+      { title: "Zet Mosoma | Fullstack Web Developer" },
       {
-        title: "TanStack Start Starter",
+        name: "description",
+        content:
+          "Fullstack web developer specializing in React, TypeScript, Node.js, Express.js, MongoDB and modern web technologies. Check out my projects and get in touch.",
       },
+      {
+        name: "keywords",
+        content:
+          "fullstack developer, frontend developer, react developer, typescript, web development,MERN Stack, portfolio",
+      },
+      { name: "author", content: "Zet Mosoma" },
+
+      // Open Graph (for link previews on social media)
+      { property: "og:type", content: "website" },
+      { property: "og:url", content: "https://yoursite.com" },
+      { property: "og:title", content: "Zet Mosoma | Fullstack Web Developer" },
+      {
+        property: "og:description",
+        content:
+          "Fullstack web developer specializing in React, TypeScript, Node.js, Express.js, MongoDB and modern web technologies. Check out my projects and get in touch.",
+      },
+      { property: "og:image", content: "https://yoursite.com/og-image.png" },
+
+      // Twitter Card
+      { name: "twitter:card", content: "summary_large_image" },
+      {
+        name: "twitter:title",
+        content: "Zet Mosoma | Fullstack Web Developer",
+      },
+      {
+        name: "twitter:description",
+        content:
+          "Fullstack web developer specializing in React, TypeScript, Node.js, Express.js, MongoDB and modern web technologies. Check out my projects and get in touch.",
+      },
+      { name: "twitter:image", content: "https://yoursite.com/website.png" },
     ],
     links: [
       {
         rel: "stylesheet",
         href: appCss,
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
+      },
+    ],
+    scripts: [
+      {
+        type: "application/ld+json",
+        children: JSON.stringify({
+          "@context": "https://schema.org",
+          "@type": "Person",
+          name: "Zet Mosoma",
+          url: "https://yoursite.com",
+          jobTitle: "Fullstack Web Developer",
+          sameAs: [
+            "https://github.com/zetmosoma10",
+            "https://www.linkedin.com/in/zetmosoma/",
+          ],
+        }),
       },
     ],
   }),
